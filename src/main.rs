d96e079f6e582fc9a2b5589571a8d5e67f362d85
erhodes <erik@space-nav.com>
2022-08-02 15:11:43 -0600

    cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use sea_orm::{Condition, DbBackend, QueryTrait};
 use sea_orm::entity::prelude::*;
+use sea_orm::{Condition, DbBackend, QueryTrait};
 
 #[derive(Clone, Debug, PartialEq, DeriveEntityModel)]
 #[sea_orm(table_name = "table")]
@@ -24,19 +24,32 @@ fn main() {
     let value = "dog";
     let id = 1000000;
     let cond_first_query = Entity::find()
-        .filter(Condition::any().add(Column::Field2.eq(value)).add(Column::Field3.eq(value)))
+        .filter(
+            Condition::any()
+                .add(Column::Field2.eq(value))
+                .add(Column::Field3.eq(value)),
+        )
         .filter(Column::Field1.eq(id))
         .into_query();
 
     let cond_second_query = Entity::find()
         .filter(Column::Field1.eq(id))
-        .filter(Condition::any().add(Column::Field2.eq(value)).add(Column::Field3.eq(value)))
+        .filter(
+            Condition::any()
+                .add(Column::Field2.eq(value))
+                .add(Column::Field3.eq(value)),
+        )
         .into_query();
 
     println!("{:?}", cond_first_query);
     println!("{:?}", cond_second_query);
 
-    println!("{}", cond_first_query.to_string(sea_orm::sea_query::MysqlQueryBuilder));
-    println!("{}", cond_second_query.to_string(sea_orm::sea_query::MysqlQueryBuilder));
-
+    println!(
+        "{}",
+        cond_first_query.to_string(sea_orm::sea_query::MysqlQueryBuilder)
+    );
+    println!(
+        "{}",
+        cond_second_query.to_string(sea_orm::sea_query::MysqlQueryBuilder)
+    );
 }
